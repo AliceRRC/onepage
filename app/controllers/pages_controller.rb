262550ee3f1e4b8b3ac90583ffd8e09277873561
Alice Ratton <aliceratton@gmail.com>
2021-04-16 14:36:46 -0300

    index page done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,23 @@
 class PagesController < ApplicationController
   def index
+    @participations = Participation.all
   end
 
   def show
   end
 
   def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
